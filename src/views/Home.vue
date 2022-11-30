0ab402e3612a237cd9122f0955406d838b90b296
Shiyam Kugaeasan <kshiyam07@gmail.com>
2022-11-29 22:47:18 -0500

    Added files for assignment 5.

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {ref} from "vue";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+const username = ref("");
+const password = ref("");
+const error = ref(false);
+
+const login = () => {
+    if (username.value === "tmdb" && password.value === "movies") {
+        router.push("./movies");
+    }
+    else {
+        error.value = true;
+    }
+};
+</script>
+
+<template>
+    <div class="home">
+        <h1>Login</h1>
+        <form @submit.prevent="login()">
+            <input type="text">
+            <input type="password">
+            <input type="submit">
+        </form>
+        <div v-if="error">
+            <p>Incorrect username or password.</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
