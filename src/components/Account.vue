27c28deadf9450901ebcff0315007d34237c4650
Shiyam Kugaeasan <kshiyam07@gmail.com>
2022-12-01 22:45:31 -0500

    Added store file.

---
+++ src/components/Account.vue
@@ -0,0 +1,15 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue'
+import {useStore} from "../store/store.js";
+
+const store = useStore();
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,15 @@
 import { defineStore } from "pinia";
+
+export const useStore = defineStore('store', {
+    state: () => {
+        return {
+            movie: "Spider-Man",
+            count: 0,
+        }
+    },
+    actions: {
+        increment() {
+            this.count++;
+        },
+    },
+});
